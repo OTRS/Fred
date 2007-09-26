fcce2f65e223c4e7f317b91a2878ef1cf438fe2d
Thomas Raith <thomas.raith@otrs.com>
2007-09-26 10:37:41 +0000

    Insert the config log.

---
+++ Kernel/Output/HTML/FredConfigLog.pm
@@ -0,0 +1,125 @@
+# --
+# Kernel/Output/HTML/FredConfigLog.pm - layout backend module
+# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# --
+# $Id: FredConfigLog.pm,v 1.1 2007-09-26 10:37:41 tr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Output::HTML::FredConfigLog;
+
+use strict;
+use warnings;
+
+use vars qw(@ISA $VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+=head1 NAME
+
+Kernel::Output::HTML::FredConfigLog - layout backend module
+
+=head1 SYNOPSIS
+
+All layout functions of the config log module
+
+=over 4
+
+=cut
+
+=item new()
+
+create a object
+
+    $BackendObject = Kernel::Output::HTML::FredConfigLog->new(
+        %Param,
+    );
+
+=cut
+
+sub new {
+    my $Type  = shift;
+    my %Param = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (qw(ConfigObject LogObject LayoutObject)) {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+=item CreateFredOutput()
+
+create the output of the translationdebugging log
+
+    $LayoutObject->CreateFredOutput(
+        ModulesRef => $ModulesRef,
+    );
+
+=cut
+
+sub CreateFredOutput {
+    my $Self      = shift;
+    my %Param     = @_;
+    my $HTMLLines = '';
+
+    # check needed stuff
+    if ( !$Param{ModuleRef} ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => "Need ModuleRef!",
+        );
+        return;
+    }
+
+    for my $Line ( @{ ${ $Param{ModuleRef} }{Data} } ) {
+        for my $TD (@{$Line}) {
+            $TD = $Self->{LayoutObject}->Ascii2Html(Text => $TD);
+        }
+        if ($Line->[1] eq 'True') {
+            $Line->[1] = '';
+        }
+        $HTMLLines .= "        <tr>\n"
+                    . "          <td align=\"right\">$Line->[2]</td>\n"
+                    . "          <td>$Line->[0]</td>\n"
+                    . "          <td>$Line->[1]</td>\n"
+                    . "        </tr>";
+    }
+
+    if ($HTMLLines) {
+        ${ $Param{ModuleRef} }{Output} = $Self->{LayoutObject}->Output(
+            TemplateFile => 'DevelFredConfigLog',
+            Data         => { HTMLLines => $HTMLLines, },
+        );
+    }
+
+    return 1;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2007-09-26 10:37:41 $
+
+=cut
---
+++ Kernel/System/Fred/ConfigLog.pm
@@ -0,0 +1,275 @@
+# --
+# Kernel/System/Fred/ConfigLog.pm
+# Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
+# --
+# $Id: ConfigLog.pm,v 1.1 2007-09-26 10:37:41 tr Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::System::Fred::ConfigLog;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+=head1 NAME
+
+Kernel::System::Fred::ConfigLog
+
+=head1 SYNOPSIS
+
+handle the config log data
+
+=over 4
+
+=cut
+
+=item new()
+
+create a object
+
+    use Kernel::Config;
+    use Kernel::System::Log;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+    );
+
+=cut
+
+sub new {
+    my $Type  = shift;
+    my %Param = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Object (qw(ConfigObject LogObject)) {
+        if ( $Param{$Object} ) {
+            $Self->{$Object} = $Param{$Object};
+        }
+    }
+
+    return $Self;
+}
+
+=item DataGet()
+
+Get the data for this fred module. Returns true or false.
+And add the data to the module ref.
+
+    $BackendObject->DataGet(
+        ModuleRef => $ModuleRef,
+    );
+
+=cut
+
+sub DataGet {
+    my $Self  = shift;
+    my %Param = @_;
+    my @LogMessages;
+
+    # open the TranslationDebug.log file to get the untranslated words
+    my $File = $Self->{ConfigObject}->Get('Home') . "/var/fred/Config.log";
+    if ( open my $Filehandle, '<', $File ) {
+        my @Row        = <$Filehandle>;
+        my @ReverseRow = reverse(@Row);
+
+        # get the whole information
+        for my $Line (@ReverseRow) {
+            if ( $Line =~ /FRED/ ) {
+                last;
+            }
+            push @LogMessages, $Line;
+        }
+        close $Filehandle;
+        pop @LogMessages;
+        $Self->InsertWord( What => "FRED\n" );
+    }
+
+    my %IndividualConfig = ();
+
+    for my $Line (@LogMessages) {
+        $Line =~ s/\n//;
+        $IndividualConfig{$Line}++;
+    }
+
+    @LogMessages = ();
+    for my $Line (keys %IndividualConfig) {
+        my @SplitedLine = split /;/, $Line;
+        push @SplitedLine, $IndividualConfig{$Line};
+        push @LogMessages, \@SplitedLine;
+    }
+
+    ${ $Param{ModuleRef} }{Data} = \@LogMessages ;
+    return 1;
+}
+
+=item ActivateModuleTodos()
+
+Do all jobs which are necessary to activate this special module.
+
+    $FredObject->ActivateModuleTodos(
+        ModuleName => $ModuleName,
+    );
+
+=cut
+
+sub ActivateModuleTodos {
+    my $Self  = shift;
+    my @Lines = ();
+
+    my $File = $Self->{ConfigObject}->Get('Home') . "/Kernel/Config/Defaults.pm";
+
+    # check if it is an symlink, because it can be development system which use symlinks
+    if ( -l "$File" ) {
+        die 'Can\'t manipulate $File because it is a symlink!';
+    }
+
+    # to use TranslationDebug I have to manipulate the Language.pm file
+    open my $Filehandle, '<', $File || die "Can't open $File !\n";
+    while ( my $Line = <$Filehandle> ) {
+        push @Lines, $Line;
+    }
+    close $Filehandle;
+
+    open my $FilehandleII, '>', $File || die "Can't write $File !\n";
+    my $SubGet = '';
+    for my $Line (@Lines) {
+        print $FilehandleII $Line;
+        if ( $Line =~ /sub Get/ ) {
+            $SubGet = "Get";
+        }
+        if ( $SubGet eq 'Get' && $Line =~ /my \$Self = shift;/ ) {
+            $SubGet .= 'Self';
+        }
+        if ( $SubGet eq 'GetSelf' && $Line =~ /my \$What = shift;/ ) {
+            print $FilehandleII "# FRED - manipulated\n";
+            print $FilehandleII "use Kernel::System::Fred::ConfigLog;\n";
+            print $FilehandleII "my \$ConfigLogObject = Kernel::System::Fred::ConfigLog->new();\n";
+            print $FilehandleII "if (\$Self->{\$What}) { # FRED - manipulated\n";
+            print $FilehandleII "    \$ConfigLogObject->InsertWord(What => \"\$What;True\", Home => \$Self->{Home});\n";
+            print $FilehandleII "}                     # FRED - manipulated\n";
+            print $FilehandleII "else {                # FRED - manipulated\n";
+            print $FilehandleII
+                "    \$ConfigLogObject->InsertWord(What => \"\$What;False\", Home => \$Self->{Home});\n";
+            print $FilehandleII "}                     # FRED - manipulated\n";
+            print $FilehandleII "# FRED - manipulated\n";
+        }
+    }
+    close $FilehandleII;
+
+    # check if the needed path is available
+    my $Path = $Self->{ConfigObject}->Get('Home') . '/var/fred';
+    if ( !-e $Path ) {
+        system "mkdir $Path";
+    }
+
+    return 1;
+}
+
+=item DeactivateModuleTodos()
+
+Do all jobs which are necessary to deactivate this special module.
+
+    $FredObject->DeactivateModuleTodos(
+        ModuleName => $ModuleName,
+    );
+
+=cut
+
+sub DeactivateModuleTodos {
+    my $Self  = shift;
+    my @Lines = ();
+    my $File  = $Self->{ConfigObject}->Get('Home') . "/Kernel/Config/Defaults.pm";
+
+    # check if it is an symlink, because it can be development system which use symlinks
+    if ( -l "$File" ) {
+        die 'Can\'t manipulate $File because it is a symlink!';
+    }
+
+    # to use TranslationDebugger I have to manipulate the Language.pm file
+    # here I undo my manipulation
+    open my $Filehandle, '<', $File || die "Can't open $File !\n";
+    while ( my $Line = <$Filehandle> ) {
+        push @Lines, $Line;
+    }
+    close $Filehandle;
+
+    open my $FilehandleII, '>', $File || die "Can't write $File !\n";
+
+    my %RemoveLine = (
+        "# FRED - manipulated\n"                                                                 => 1,
+        "use Kernel::System::Fred::ConfigLog;\n"                                                 => 1,
+        "my \$ConfigLogObject = Kernel::System::Fred::ConfigLog->new();\n"                       => 1,
+        "if (\$Self->{\$What}) { # FRED - manipulated\n"                                         => 1,
+        "    \$ConfigLogObject->InsertWord(What => \"\$What;True\", Home => \$Self->{Home});\n"  => 1,
+        "}                     # FRED - manipulated\n"                                           => 1,
+        "else {                # FRED - manipulated\n"                                           => 1,
+        "    \$ConfigLogObject->InsertWord(What => \"\$What;False\", Home => \$Self->{Home});\n" => 1,
+    );
+
+    for my $Line (@Lines) {
+        if ( !$RemoveLine{$Line} ) {
+            print $FilehandleII $Line;
+        }
+    }
+    close $FilehandleII;
+    return 1;
+}
+
+=item InsertWord()
+
+Save a word in the translation debug log
+
+    $BackendObject->InsertWord(
+        What => 'a word',
+    );
+
+=cut
+
+sub InsertWord {
+    my $Self  = shift;
+    my %Param = @_;
+
+    if ( !$Param{Home} ) {
+        $Param{Home} = $Self->{ConfigObject}->Get('Home');
+    }
+
+    # save the word in log file
+    my $File = $Param{Home} . '/var/fred/Config.log';
+    open my $Filehandle, '>>', $File || die "Can't write $File !\n";
+    print $Filehandle $Param{What} . "\n";
+    close $Filehandle;
+
+    return 1;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (http://otrs.org/).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+
+=cut
+
+=head1 VERSION
+
+$Revision: 1.1 $ $Date: 2007-09-26 10:37:41 $
+
+=cut
