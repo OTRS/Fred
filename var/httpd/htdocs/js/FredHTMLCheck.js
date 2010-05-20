5659462c50d829d7e02b8bd1450634900d91260c
Martin Gruner <martin.gruner@otrs.com>
2010-05-20 11:41:37 +0000

    Added more accessibility checks.

--- var/httpd/htdocs/js/FredHTMLCheck.js
+++ var/httpd/htdocs/js/FredHTMLCheck.js
@@ -39,33 +39,71 @@ OTRS.Fred.HTMLCheck = (function (TargetNS) {
      *      nothing, but calls OutputError if an error was found
      */
 
-    function CheckAccessibilityLabel() {
+    function CheckAccessibility() {
+        /*
+         * check if input elements either have a label or an assigned title text
+         */
         $('input:text, input: password, input:checkbox, input:radio, select, textarea').each(function(){
-            // first check if a title attribute is present, that is also ok for accessibility
-            var Title = $(this).attr('title');
-            if (Title && Title.length) return;
+            var $this = $(this),
+                Label = $([]);
 
-            // ok, no title available, now look for an assigned label element
-            var Label = $('label[for=' + $(this).attr('id') + ']');
-            if (!Label || !Label.length) {
-                OutputError(
-                    $(this),
-                    'AccessibilityMissingLabel',
-                    'Input element without a describing label or title attribute',
-                    'Please add a title attribute or a label element with a "speaking" description for this element.'
-                );
+            if ($this.attr('id') && $this.attr('id').length) {
+                Label = $('label[for=' + $this.attr('id')  + ']');
             }
+
             if (Label.length > 1) {
                 OutputError(
-                    $(this),
+                    $this,
                     'AccessibilityMultipleLabel',
                     'Input element with more than one assigned labels',
                     'Please make sure that only one label is present for this input element.'
                 );
             }
+
+            // first check if a title attribute is present, that is also ok for accessibility
+            var Title = $this.attr('title');
+            if (Title && Title.length) {
+                return;
+            }
+
+            // ok, no title available, now look for an assigned label element
+            if (!Label || !Label.length) {
+                OutputError(
+                    $this,
+                    'AccessibilityMissingLabel',
+                    'Input element without a describing label or title attribute',
+                    'Please add a title attribute or a label element with a "speaking" description for this element.'
+                );
+            }
+        });
+
+        /*
+         * check if links have either a text or a title
+         */
+        $('a').each(function(){
+            var $this = $(this);
+
+            if ($this.attr('name') && !$this.attr('href')) {
+                return;
+            }
+
+            if ($this.text() && $this.text().length) {
+                return;
+            }
+            if ($this.attr('title') && $this.attr('title').length) {
+                return;
+            }
+
+            OutputError(
+                $this,
+                'AccessibilityInaccessibleLink',
+                'Link without text or title',
+                'Please make sure that every link has either a text content or a title attribute that can be used by a screenreader to identify the link.'
+            );
+
         });
     }
-    CheckFunctions.push(CheckAccessibilityLabel);
+    CheckFunctions.push(CheckAccessibility);
 
     /**
      * @function
@@ -95,6 +133,9 @@ OTRS.Fred.HTMLCheck = (function (TargetNS) {
         $.each(CheckFunctions, function(){
             this();
         });
+        if ($('#FredHTMLCheckResults').text() === '') {
+            $('#FredHTMLCheckResults').html('<p class="Confirmation">All checks ok.</p>');
+        }
     };
 
     return TargetNS;
