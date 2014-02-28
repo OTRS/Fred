84ee686153658934ea3d5361e5d5b9ea6dfd48b0
Martin Gruner <martin.gruner@otrs.com>
2014-02-28 10:38:07 +0100

    Fixed TranslationDebug to no longer patch Language.pm.

--- Kernel/Config/Files/AAAFred.pm
+++ Kernel/Config/Files/AAAFred.pm
@@ -12,6 +12,9 @@ use warnings;
 
 use vars qw($Self);
 
+use Kernel::Language;
+use Kernel::System::Fred::TranslationDebug;
+
 if ( $ENV{HTTP_USER_AGENT} ) {
 
     # check if the needed path is available
@@ -30,6 +33,35 @@ if ( $ENV{HTTP_USER_AGENT} ) {
     # move STDOUT to tmp file
     if ( !open STDERR, '>>', $File ) { ## no critic
         print STDERR "ERROR: Can't write $File!";
+    }
+}
+
+# disable redefine warnings in this scope
+{
+    no warnings 'redefine';
+
+    # Override Kernel::Language::Get() method to intercept missing translations
+    if ( Kernel::Language->can('Get') && !Kernel::Language->can('GetOriginal') ) {
+        *Kernel::Language::GetOriginal = \&Kernel::Language::Get;
+        *Kernel::Language::Get = sub {
+            my ($Self, $What) = @_;
+
+            return if !defined $What;
+            return '' if $What eq '';
+
+            my $Result = $Self->GetOriginal($What);
+
+            if ( $What && $What =~ /^(.+?)",\s{0,1}"(.*?)$/ ) {
+                $What = $1;
+            }
+
+            if (!$Self->{Translation}->{$What}) {
+                $Self->{TranslationDebugObject} //= Kernel::System::Fred::TranslationDebug->new(%{$Self});
+                $Self->{TranslationDebugObject}->InsertWord(What => $What);
+            }
+
+            return $Result;
+        };
     }
 }
 
