529679681f53bdd4e39aabd23fa31ff4ba9164fe
Martin Gruner <martin.gruner@otrs.com>
2014-03-04 12:46:01 +0100

    Fixed a bug that caused a ring reference in ConfigLog.

--- Kernel/System/Fred/ConfigLog.pm
+++ Kernel/System/Fred/ConfigLog.pm
@@ -13,6 +13,8 @@ package Kernel::System::Fred::ConfigLog;
 use strict;
 use warnings;
 
+use Scalar::Util();
+
 =head1 NAME
 
 Kernel::System::Fred::ConfigLog
@@ -60,6 +62,9 @@ sub new {
     for my $Object (qw(ConfigObject)) {
         $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
     }
+    # ConfigObject holds a reference to us, so don't reference it to avoid
+    #   a ring reference.
+    Scalar::Util::weaken($Self->{ConfigObject});
 
     # Don't call ConfigObject->Get() here, this could cause deep recursions.
 
