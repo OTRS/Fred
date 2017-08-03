7ed69a9a8b92d44de46ff33a3f3bc2c7668999c3
Martin Gruner <martin.gruner@otrs.com>
2017-08-03 14:50:03 +0200

    Fixed Fred Perl configuration file to implement the new format correctly.

--- Kernel/Config/Files/AAAFred.pm
+++ Kernel/Config/Files/AAAFred.pm
@@ -25,126 +25,132 @@ use Kernel::System::Fred::TranslationDebug;
 
 our $ObjectManagerDisabled = 1;
 
-sub Load { }
+sub Load {
+    my ($File, $Self) = @_;
 
-if ( $ENV{HTTP_USER_AGENT} ) {
+    if ( $ENV{HTTP_USER_AGENT} ) {
 
-    # check if the needed path is available
-    my $Path = $Self->{Home} . '/var/fred';
-    if ( !-e $Path ) {
-        mkdir $Path;
+        # check if the needed path is available
+        my $Path = $Self->{Home} . '/var/fred';
+        if ( !-e $Path ) {
+            mkdir $Path;
+        }
+
+        my $File = $Self->{Home} . '/var/fred/STDERR.log';
+
+        # check log file size
+        if ( -s $File > 20 * 1024 * 1024 ) {
+            unlink $File;
+        }
+
+        # move STDOUT to tmp file
+        if ( !open STDERR, '>>', $File ) {    ## no critic
+            print STDERR "ERROR: Can't write $File!";
+        }
     }
 
-    my $File = $Self->{Home} . '/var/fred/STDERR.log';
+    # disable redefine warnings in this scope
+    {
+        no warnings 'redefine';               ## no critic
 
-    # check log file size
-    if ( -s $File > 20 * 1024 * 1024 ) {
-        unlink $File;
-    }
+        # Override Kernel::Language::Get() method to intercept missing translations
+        if ( Kernel::Language->can('Get') && !Kernel::Language->can('GetOriginal') ) {
+            *Kernel::Language::GetOriginal = \&Kernel::Language::Get;
+            *Kernel::Language::Get         = sub {
+                my ( $Self, $What ) = @_;
 
-    # move STDOUT to tmp file
-    if ( !open STDERR, '>>', $File ) {    ## no critic
-        print STDERR "ERROR: Can't write $File!";
+                return if !defined $What;
+                return '' if $What eq '';
+
+                my $Result = $Self->GetOriginal($What);
+
+                if ( $What && $What =~ /^(.+?)",\s{0,1}"(.*?)$/ ) {
+                    $What = $1;
+                }
+
+                if ( !$Self->{Translation}->{$What} ) {
+                    $Self->{TranslationDebugObject} ||= Kernel::System::Fred::TranslationDebug->new();
+                    $Self->{TranslationDebugObject}->InsertWord( What => $What );
+                }
+
+                return $Result;
+            };
+        }
+
+        # Override Kernel::Language::Translate() method to intercept missing translations
+        if ( Kernel::Language->can('Translate') && !Kernel::Language->can('TranslateOriginal') ) {
+            *Kernel::Language::TranslateOriginal = \&Kernel::Language::Translate;
+            *Kernel::Language::Translate         = sub {
+                my ( $Self, $Text, @Parameters ) = @_;
+
+                if ( $Text && !$Self->{Translation}->{$Text} ) {
+                    $Self->{TranslationDebugObject} ||= Kernel::System::Fred::TranslationDebug->new();
+                    $Self->{TranslationDebugObject}->InsertWord( What => $Text );
+                }
+
+                return $Self->TranslateOriginal( $Text, @Parameters );
+            };
+        }
+
+        # Override Kernel::System::DB::Prepare() method to intercept database calls
+        if ( Kernel::System::DB->can('Prepare') && !Kernel::System::DB->can('PrepareOriginal') ) {
+            *Kernel::System::DB::PrepareOriginal = \&Kernel::System::DB::Prepare;
+            *Kernel::System::DB::Prepare         = sub {
+                my ( $Self, %Param ) = @_;
+
+                $Self->{SQLLogObject} ||= Kernel::System::Fred::SQLLog->new();
+                $Self->{SQLLogObject}->PreStatement(%Param);
+                my $Result = $Self->PrepareOriginal(%Param);
+                $Self->{SQLLogObject}->PostStatement(%Param);
+
+                return $Result;
+            };
+        }
+
+        # Override Kernel::System::DB::Do() method to intercept database calls
+        if ( Kernel::System::DB->can('Do') && !Kernel::System::DB->can('DoOriginal') ) {
+            *Kernel::System::DB::DoOriginal = \&Kernel::System::DB::Do;
+            *Kernel::System::DB::Do         = sub {
+                my ( $Self, %Param ) = @_;
+
+                $Self->{SQLLogObject} ||= Kernel::System::Fred::SQLLog->new();
+                $Self->{SQLLogObject}->PreStatement(%Param);
+                my $Result = $Self->DoOriginal(%Param);
+                $Self->{SQLLogObject}->PostStatement(%Param);
+
+                return $Result;
+            };
+        }
+
+        # Override Kernel::Config::Get() method to intercept config strings
+        if ( Kernel::Config::Defaults->can('Get') && !Kernel::Config::Defaults->can('GetOriginal') ) {
+            *Kernel::Config::Defaults::GetOriginal = \&Kernel::Config::Defaults::Get;
+            *Kernel::Config::Defaults::Get         = sub {
+                my ( $Self, $What ) = @_;
+
+                $Self->{ConfigLogObject} ||= Kernel::System::Fred::ConfigLog->new();
+                my $Caller = caller();
+                if ( $Self->{$What} ) {
+                    $Self->{ConfigLogObject}->InsertWord(
+                        What => "$What;True;$Caller;",
+                        Home => $Self->{Home}
+                    );
+                }
+                else {
+                    $Self->{ConfigLogObject}->InsertWord(
+                        What => "$What;False;$Caller;",
+                        Home => $Self->{Home}
+                    );
+                }
+
+                return $Self->GetOriginal($What);
+            };
+        }
     }
+    return;
 }
 
-# disable redefine warnings in this scope
-{
-    no warnings 'redefine';               ## no critic
 
-    # Override Kernel::Language::Get() method to intercept missing translations
-    if ( Kernel::Language->can('Get') && !Kernel::Language->can('GetOriginal') ) {
-        *Kernel::Language::GetOriginal = \&Kernel::Language::Get;
-        *Kernel::Language::Get         = sub {
-            my ( $Self, $What ) = @_;
 
-            return if !defined $What;
-            return '' if $What eq '';
-
-            my $Result = $Self->GetOriginal($What);
-
-            if ( $What && $What =~ /^(.+?)",\s{0,1}"(.*?)$/ ) {
-                $What = $1;
-            }
-
-            if ( !$Self->{Translation}->{$What} ) {
-                $Self->{TranslationDebugObject} ||= Kernel::System::Fred::TranslationDebug->new();
-                $Self->{TranslationDebugObject}->InsertWord( What => $What );
-            }
-
-            return $Result;
-        };
-    }
-
-    # Override Kernel::Language::Translate() method to intercept missing translations
-    if ( Kernel::Language->can('Translate') && !Kernel::Language->can('TranslateOriginal') ) {
-        *Kernel::Language::TranslateOriginal = \&Kernel::Language::Translate;
-        *Kernel::Language::Translate         = sub {
-            my ( $Self, $Text, @Parameters ) = @_;
-
-            if ( $Text && !$Self->{Translation}->{$Text} ) {
-                $Self->{TranslationDebugObject} ||= Kernel::System::Fred::TranslationDebug->new();
-                $Self->{TranslationDebugObject}->InsertWord( What => $Text );
-            }
-
-            return $Self->TranslateOriginal( $Text, @Parameters );
-        };
-    }
-
-    # Override Kernel::System::DB::Prepare() method to intercept database calls
-    if ( Kernel::System::DB->can('Prepare') && !Kernel::System::DB->can('PrepareOriginal') ) {
-        *Kernel::System::DB::PrepareOriginal = \&Kernel::System::DB::Prepare;
-        *Kernel::System::DB::Prepare         = sub {
-            my ( $Self, %Param ) = @_;
-
-            $Self->{SQLLogObject} ||= Kernel::System::Fred::SQLLog->new();
-            $Self->{SQLLogObject}->PreStatement(%Param);
-            my $Result = $Self->PrepareOriginal(%Param);
-            $Self->{SQLLogObject}->PostStatement(%Param);
-
-            return $Result;
-        };
-    }
-
-    # Override Kernel::System::DB::Do() method to intercept database calls
-    if ( Kernel::System::DB->can('Do') && !Kernel::System::DB->can('DoOriginal') ) {
-        *Kernel::System::DB::DoOriginal = \&Kernel::System::DB::Do;
-        *Kernel::System::DB::Do         = sub {
-            my ( $Self, %Param ) = @_;
-
-            $Self->{SQLLogObject} ||= Kernel::System::Fred::SQLLog->new();
-            $Self->{SQLLogObject}->PreStatement(%Param);
-            my $Result = $Self->DoOriginal(%Param);
-            $Self->{SQLLogObject}->PostStatement(%Param);
-
-            return $Result;
-        };
-    }
-
-    # Override Kernel::Config::Get() method to intercept config strings
-    if ( Kernel::Config::Defaults->can('Get') && !Kernel::Config::Defaults->can('GetOriginal') ) {
-        *Kernel::Config::Defaults::GetOriginal = \&Kernel::Config::Defaults::Get;
-        *Kernel::Config::Defaults::Get         = sub {
-            my ( $Self, $What ) = @_;
-
-            $Self->{ConfigLogObject} ||= Kernel::System::Fred::ConfigLog->new();
-            my $Caller = caller();
-            if ( $Self->{$What} ) {
-                $Self->{ConfigLogObject}->InsertWord(
-                    What => "$What;True;$Caller;",
-                    Home => $Self->{Home}
-                );
-            }
-            else {
-                $Self->{ConfigLogObject}->InsertWord(
-                    What => "$What;False;$Caller;",
-                    Home => $Self->{Home}
-                );
-            }
-
-            return $Self->GetOriginal($What);
-        };
-    }
-}
 
 1;
